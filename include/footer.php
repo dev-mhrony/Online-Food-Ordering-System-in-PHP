878e3f0a007ef7e2887adea1d3fe93818b608155
dev-mhrony <mh.rony.dhanvi@gmail.com>
2023-02-22 13:37:18 +0600

    Last update - 22-02-2023

--- include/footer.php
+++ include/footer.php
@@ -38,3 +38,14 @@
 
           </div>
       </footer>
+
+
+      <!-- /*!
+ *  Author Name: MH RONY.
+ *  GigHub Link: https://github.com/dev-mhrony
+ *  Facebook Link:https://www.facebook.com/dev.mhrony
+ *  Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+    for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
+ *  Visit My Website : developerrony.com 
+
+      */ -->
