55873c7a3fecc050cb86edc382634ad5bb34f189
dev-mhrony <mh.rony.dhanvi@gmail.com>
2023-02-22 14:02:17 +0600

    Last update - 22-02-2023

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,1 +1,10 @@
-<footer class="footer">Code Camp BD © 2023 - Online Food Ordering System Developer - <a href="https://www.facebook.com/dev.mhrony">MH RONY</a> </footer>
+ <footer class="footer">Code Camp BD © 2023 - Online Food Ordering System Developer - <a href="https://www.facebook.com/dev.mhrony">MH RONY</a> </footer>
+ <!--/*!
+ * Author Name: MH RONY.
+ * GigHub Link: https://github.com/dev-mhrony
+ * Facebook Link:https://www.facebook.com/dev.mhrony
+ * Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+ for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com
+ * Visit My Website : developerrony.com
+
+ */ -->
