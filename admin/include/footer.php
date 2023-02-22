e8e02b5d2e5b4840053e3be413a085459b237804
dev-mhrony <mh.rony.dhanvi@gmail.com>
2023-02-22 13:44:31 +0600

    Last update - 22-02-2023

---
+++ admin/include/footer.php
@@ -0,0 +1,1 @@
+<footer class="footer">Code Camp BD © 2023 - Online Food Ordering System Developer - <a href="https://www.facebook.com/dev.mhrony">MH RONY</a> </footer>
--- connection/connect.php
+++ connection/connect.php
@@ -1,5 +1,13 @@
 <?php
+/*!
+ *  Author Name: MH RONY.
+ *  GigHub Link: https://github.com/dev-mhrony
+ *  Facebook Link:https://www.facebook.com/dev.mhrony
+ *  Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+    for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
+ *  Visit My Website : developerrony.com 
 
+      */
 //main connection file for both admin & front end
 $servername = "localhost"; //server
 $username = "root"; //username
@@ -12,5 +20,13 @@ $db = mysqli_connect($servername, $username, $password, $dbname); // connecting
 if (!$db) {       //checking connection to DB	
     die("Connection failed: " . mysqli_connect_error());
 }
+/*!
+ *  Author Name: MH RONY.
+ *  GigHub Link: https://github.com/dev-mhrony
+ *  Facebook Link:https://www.facebook.com/dev.mhrony
+ *  Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+    for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
+ *  Visit My Website : developerrony.com 
 
+      */
 ?>
